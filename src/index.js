75eed79e4bbff9585c766ec9455da98217891a7c
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-09-02 12:23:21 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import './App.css';
-import About from "./Components/About";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-import Contacts from "./Components/Contacts";
-import Testimonial from "./Components/Testimonial";
 
-import Home from "./Components/Home";
-import Work from "./Components/Work";
-import { themeContext, ThemeContextProvider } from "./contextProviders/ThemeContextProvider";
+import {  ThemeContextProvider } from "./contextProviders/ThemeContextProvider";
 
 
 
